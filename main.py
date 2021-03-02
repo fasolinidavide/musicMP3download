060713417b454030e7065ea200c7b9b1e89feb57
jacksk <73524591+jacksk@users.noreply.github.com>
2021-03-02 18:17:06 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import youtube_dl #scaricare la youtuble_dl
+import shutil, os
+
+link = input("inserisci il link della canzone \n")
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(([link]))
+try:
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            shutil.move(file, r"C:\Users\Admin\Desktop\canzoni") #scegliere il file dove mettere
+                                                                 #la propria canzone
+except:
+    print("file già esistente")
+    os.remove(file)
+
+
